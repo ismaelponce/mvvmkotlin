bda02714ed7532d170229f9dd47761081509b171
ismaelponce <43687200+ismaelponce@users.noreply.github.com>
2021-11-30 17:40:23 +0100

    v3

--- app/src/main/java/com/example/listadofacturasmvvmkotlin/presentation/listadofactura/FilterViewActivity.kt
+++ app/src/main/java/com/example/listadofacturasmvvmkotlin/presentation/listadofactura/FilterViewActivity.kt
@@ -1,167 +1,155 @@
-package com.example.listadofacturasmvvmkotlin.presentation.listadofactura;
+package com.example.listadofacturasmvvmkotlin.presentation.listadofactura
 
-import static com.example.listadofacturasmvvmkotlin.utils.Constants.*;
-import static com.example.listadofacturasmvvmkotlin.utils.Constants.DESDE_DATE_PICKER_TAG;
-import static com.example.listadofacturasmvvmkotlin.utils.Constants.HASTA_DATE_PICKER_TAG;
-import static com.example.listadofacturasmvvmkotlin.utils.Constants.KEYMAX;
-import static com.example.listadofacturasmvvmkotlin.utils.Constants.KEYWRAPPER;
+import androidx.appcompat.app.AppCompatActivity
+import com.example.listadofacturasmvvmkotlin.data.model.FiltersWrapper
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.SeekBar
+import android.content.Intent
+import com.google.gson.Gson
+import android.app.Activity
+import com.example.listadofacturasmvvmkotlin.presentation.listadofactura.DatePickerFragment
+import android.app.DatePickerDialog.OnDateSetListener
+import android.view.View
+import android.widget.DatePicker
+import com.example.listadofacturasmvvmkotlin.databinding.ActivityFilterViewBinding
+import com.example.listadofacturasmvvmkotlin.utils.Constants.DESDE_DATE_PICKER_TAG
+import com.example.listadofacturasmvvmkotlin.utils.Constants.HASTA_DATE_PICKER_TAG
+import com.example.listadofacturasmvvmkotlin.utils.Constants.KEYMAX
+import com.example.listadofacturasmvvmkotlin.utils.Constants.KEYWRAPPER
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.SeekBar;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.listadofacturasmvvmkotlin.data.model.FiltersWrapper;
-import com.example.listadofacturasmvvmkotlin.databinding.ActivityFilterViewBinding;
-import com.google.gson.Gson;
-
-public class FilterViewActivity extends AppCompatActivity {
-
-    private FiltersWrapper myWrapper = new FiltersWrapper();
-    private ActivityFilterViewBinding binding;
-    private String desdeMaxDate = "";
-    private String hastaMinDate = "";
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = ActivityFilterViewBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+class FilterViewActivity : AppCompatActivity() {
+    private var myWrapper = FiltersWrapper()
+    private var binding: ActivityFilterViewBinding? = null
+    private var desdeMaxDate = ""
+    private var hastaMinDate = ""
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFilterViewBinding.inflate(
+            layoutInflater
+        )
+        setContentView(binding!!.root)
 
         // Se crea la seekbar
-        createSeekbar();
+        createSeekbar()
 
         // Se observan los valores que puedan venir de otra actividad
-        checkIntents();
+        checkIntents()
 
         // Se añaden los listeners a los botones y los datepicker
-        bindListeners();
+        bindListeners()
     }
 
-    private void bindListeners() {
-
-        binding.filterToolbar.closeButton.setOnClickListener(v -> finish());
-
-        binding.etDesde.setOnClickListener( v -> showDatePickerDialog(DESDE_DATE_PICKER_TAG, (EditText) v));
-
-        binding.etHasta.setOnClickListener ( v -> showDatePickerDialog(HASTA_DATE_PICKER_TAG, (EditText) v ));
-
-        binding.quitarButton.setOnClickListener (v -> quitFilters());
-        binding.aplicarButton.setOnClickListener (v -> applyFilters());
-
+    private fun bindListeners() {
+        binding!!.filterToolbar.closeButton.setOnClickListener { v: View? -> finish() }
+        binding!!.etDesde.setOnClickListener { v: View ->
+            showDatePickerDialog(
+                DESDE_DATE_PICKER_TAG,
+                v as EditText
+            )
+        }
+        binding!!.etHasta.setOnClickListener { v: View ->
+            showDatePickerDialog(
+                HASTA_DATE_PICKER_TAG,
+                v as EditText
+            )
+        }
+        binding!!.quitarButton.setOnClickListener { v: View? -> quitFilters() }
+        binding!!.aplicarButton.setOnClickListener { v: View? -> applyFilters() }
     }
 
-    private void createSeekbar() {
-        binding.seekbarImporte.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                String progressString = String.valueOf(progress);
-                binding.tvImporteDinamico.setText(progressString);
+    private fun createSeekbar() {
+        binding!!.seekbarImporte.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                val progressString = progress.toString()
+                binding!!.tvImporteDinamico.text = progressString
             }
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
                 // Se comenta metodo implementado por necesidad de interfaz pero innecesario
             }
 
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
                 // Se comenta metodo implementado por necesidad de interfaz pero innecesario
             }
-        });
+        })
     }
 
-    public void checkIntents(){
-        Intent intent = getIntent();
-        int importeMaximo = intent.getIntExtra(KEYMAX, 0);
-
+    fun checkIntents() {
+        val intent = intent
+        val importeMaximo = intent.getIntExtra(KEYMAX, 0)
         if (importeMaximo > 0) {
-            binding.seekbarImporte.setMax(importeMaximo);
+            binding!!.seekbarImporte.max = importeMaximo
         }
-
-        myWrapper = new Gson().fromJson(intent.getStringExtra(KEYWRAPPER), FiltersWrapper.class);
-
-        binding.etDesde.setText(myWrapper.getDesde());
-        binding.etHasta.setText(myWrapper.getHasta());
-        binding.seekbarImporte.setProgress(myWrapper.getMax());
-        binding.cbPagadas.setChecked(myWrapper.isPagadas());
-        binding.cbAnuladas.setChecked(myWrapper.isAnuladas());
-        binding.cbCuotaFija.setChecked(myWrapper.isCuotaFija());
-        binding.cbPendientesDePago.setChecked(myWrapper.isPendientesPago());
-        binding.cbPlanDePago.setChecked(myWrapper.isPlanPago());
-
+        myWrapper = Gson().fromJson(intent.getStringExtra(KEYWRAPPER), FiltersWrapper::class.java)
+        binding!!.etDesde.setText(myWrapper.desde)
+        binding!!.etHasta.setText(myWrapper.hasta)
+        binding!!.seekbarImporte.progress = myWrapper.max
+        binding!!.cbPagadas.isChecked = myWrapper.isPagadas
+        binding!!.cbAnuladas.isChecked = myWrapper.isAnuladas
+        binding!!.cbCuotaFija.isChecked = myWrapper.isCuotaFija
+        binding!!.cbPendientesDePago.isChecked = myWrapper.isPendientesPago
+        binding!!.cbPlanDePago.isChecked = myWrapper.isPlanPago
     }
 
-    private void quitFilters() {
-        binding.etDesde.setText("");
-        binding.etHasta.setText("");
-        binding.seekbarImporte.setProgress(0);
-        binding.cbPagadas.setChecked(false);
-        binding.cbAnuladas.setChecked(false);
-        binding.cbCuotaFija.setChecked(false);
-        binding.cbPendientesDePago.setChecked(false);
-        binding.cbPlanDePago.setChecked(false);
+    private fun quitFilters() {
+        binding!!.etDesde.setText("")
+        binding!!.etHasta.setText("")
+        binding!!.seekbarImporte.progress = 0
+        binding!!.cbPagadas.isChecked = false
+        binding!!.cbAnuladas.isChecked = false
+        binding!!.cbCuotaFija.isChecked = false
+        binding!!.cbPendientesDePago.isChecked = false
+        binding!!.cbPlanDePago.isChecked = false
     }
 
-    private void applyFilters() {
-        String desde = binding.etDesde.getText().toString();
-        String hasta = binding.etHasta.getText().toString();
-        String stringMax = binding.tvImporteDinamico.getText().toString();
-        int max = Integer.parseInt(stringMax);
-        boolean pagadas = binding.cbPagadas.isChecked();
-        boolean anuladas = binding.cbAnuladas.isChecked();
-        boolean cuotaFija = binding.cbCuotaFija.isChecked();
-        boolean pendientesPago = binding.cbPendientesDePago.isChecked();
-        boolean planPago = binding.cbPlanDePago.isChecked();
-
-        myWrapper = new FiltersWrapper(
-                desde,
-                hasta,
-                max,
-                pagadas,
-                anuladas,
-                cuotaFija,
-                pendientesPago,
-                planPago
-        );
-
-        String myWrapperParsed = new Gson().toJson(myWrapper);
-
-        Intent result = new Intent();
-        result.putExtra(KEYWRAPPER, myWrapperParsed);
-
-        setResult(RESULT_OK, result);
-        finish();
+    private fun applyFilters() {
+        val desde = binding!!.etDesde.text.toString()
+        val hasta = binding!!.etHasta.text.toString()
+        val stringMax = binding!!.tvImporteDinamico.text.toString()
+        val max = stringMax.toInt()
+        val pagadas = binding!!.cbPagadas.isChecked
+        val anuladas = binding!!.cbAnuladas.isChecked
+        val cuotaFija = binding!!.cbCuotaFija.isChecked
+        val pendientesPago = binding!!.cbPendientesDePago.isChecked
+        val planPago = binding!!.cbPlanDePago.isChecked
+        myWrapper = FiltersWrapper(
+            desde,
+            hasta,
+            max,
+            pagadas,
+            anuladas,
+            cuotaFija,
+            pendientesPago,
+            planPago
+        )
+        val myWrapperParsed = Gson().toJson(myWrapper)
+        val result = Intent()
+        result.putExtra(KEYWRAPPER, myWrapperParsed)
+        setResult(RESULT_OK, result)
+        finish()
     }
 
-    private void showDatePickerDialog(String tag, EditText v){
-
-        DatePickerFragment datePicker = DatePickerFragment.newInstance((view, year, month, dayOfMonth) -> {
-
-            int months = month + 1;
-            String date = dayOfMonth + "/" + months + "/" + year;
-
-            if (v.getId() == binding.etHasta.getId()) {
-                desdeMaxDate = date;
+    private fun showDatePickerDialog(tag: String, v: EditText) {
+        val datePicker =
+            DatePickerFragment.newInstance { view: DatePicker?, year: Int, month: Int, dayOfMonth: Int ->
+                val months = month + 1
+                val date = "$dayOfMonth/$months/$year"
+                if (v.id == binding!!.etHasta.id) {
+                    desdeMaxDate = date
+                }
+                if (v.id == binding!!.etDesde.id) {
+                    hastaMinDate = date
+                }
+                v.setText(date)
             }
-            if (v.getId() == binding.etDesde.getId()) {
-                hastaMinDate = date;
-            }
-
-            v.setText(date);
-
-        });
-
-        if (v.getId() == binding.etDesde.getId() && !desdeMaxDate.isEmpty()) {
-            datePicker.setDesdeMaxDate(desdeMaxDate);
+        if (v.id == binding!!.etDesde.id && !desdeMaxDate.isEmpty()) {
+            datePicker.setDesdeMaxDate(desdeMaxDate)
         }
-
-        if (v.getId() == binding.etHasta.getId() && !hastaMinDate.isEmpty()) {
-            datePicker.setHastaMinDate(hastaMinDate);
+        if (v.id == binding!!.etHasta.id && !hastaMinDate.isEmpty()) {
+            datePicker.setHastaMinDate(hastaMinDate)
         }
-
-        datePicker.show(getSupportFragmentManager(), tag);
+        datePicker.show(supportFragmentManager, tag)
     }
 }
